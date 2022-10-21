145b462811d75be11340d671df0af5a576bf01a9
Xabier Echezurieta Elduayen <xechezurieta@Xabiers-MacBook-Pro.local>
2022-10-21 18:05:07 +0200

    first commit

---
+++ api/app.js
@@ -0,0 +1,22 @@
+
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+/* const dbConnect = require('./config/mongo') */
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3001
+
+/* app.use('/api/acogidas', require('./routes/acogidas'))
+app.use('/api/auth', require('./routes/auth')) */
+
+app.listen(port, () => {
+  console.log('Tu app esta hola por http://localhost'+port)
+})
+
+/* dbConnect() */
