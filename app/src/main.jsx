fc94d8c254c42237139cfc74c3d7e7945c1e3050
Xabier Echezurieta <xechezurieta@gmail.com>
2023-03-27 22:14:13 +0200

    button ui

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './styles/index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './pages/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login/>
   </React.StrictMode>,
 )
---
+++ app/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import Button from "../ui/buttons/Button"
+
+function Login() {
+	return (
+		<div>
+			<button>Hola</button>
+			<Button kind="secondary" disabled> Hola que tal
+			</Button>
+
+		</div>
+	)
+  }
+export default Login
+  
