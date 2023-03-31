b1abd66b9c60f621bb6bd46db76fa0ade1611f65
Xabier Echezurieta <xechezurieta@gmail.com>
2023-03-31 20:31:50 +0200

    add select ui component

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -1,16 +1,18 @@
 import Button from "../ui/buttons/Button"
 import InputCheckBox from "../ui/forms/InputCheckbox"
 import InputSearch from "../ui/forms/InputSearch"
-import SearchIcon from "../ui/icons/SearchIcon"
-
+import Select from "../ui/forms/Select"
 
 function Login() {
 	return (
 		<div style={{margin: '20px'}}>
 			<Button type="primary" style={{margin: '20px'}}>Hola que tal</Button>
-			<InputSearch placeholder='Buscar...'
-			></InputSearch>
+			<InputSearch placeholder='Buscar...'></InputSearch>
 			<InputCheckBox style={{margin: '20px'}}/>
+			<Select>
+				<option value="0">Por activación</option>
+				<option value="1">Por rol</option>
+			</Select>
 		</div>
 	)
   }
---
+++ app/src/ui/forms/Select.jsx
@@ -0,0 +1,12 @@
+import style from '../forms/Select.module.css'
+import ArrowDownIcon from '../icons/ArrowDownIcon'
+
+const Select = ({className , ...props}) => (
+  <div className={`${style.wrapper} ${style.className || ''}`}>
+    <select {...props} className={style.select} ></select>
+    <ArrowDownIcon className={style.arrow}/>
+  </div>
+)
+
+
+export default Select
